0b3e5b840f443372c20d6c3cd427eb31ed3738b1
Jostein Lysberg <jostein.lysberg@gmail.com>
2020-03-05 14:05:40 +0100

    Vi har lagd FSM for første gang <3

--- lib/FSM.ex
+++ lib/FSM.ex
@@ -1,41 +1,42 @@
 defmodule FSM do
-	def _check_all_buttons(heisPID, floors, button_types) do
-		cond do 
-			floors == [] ->
-				{nil, :no_orders}
+	use GenServer
 
-			button_types == [] ->
-				[_head | tail] = floors
-				{_floor_pressed, _button_type_pressed} = _check_all_buttons(heisPID, tail, [:hall_up, :hall_down, :cab])
-
-			true ->
-				[order_head | order_tail] = button_types
-				[floor_head | _floor_tail] = floors
-				order = Driver.get_order_button_state(heisPID,floor_head, order_head)
-				if order != 1 do
-					{_floor_pressed, _button_type_pressed} = _check_all_buttons(heisPID, floors, order_tail)
-				else
-					{floor_head, order_head}
-				end
-		end
+	def start_link([]) do
+		GenServer.start_link(__MODULE__, [], name: __MODULE__)
 	end
 
-	def get_orders(heisPID) do
-		floors = [0, 1, 2, 3]
-		order_types = [:hall_up, :hall_down, :cab]
-		{floor, order_type} = _check_all_buttons(heisPID, floors, order_types)
-		if floor != nil do
-			IO.inspect({floor, order_type})
-		end
-		get_orders(heisPID)
+	def init([]) do #structen vår er {floor(siste etasje), goal_floor, direction}
+		{:ok, {nil, nil, :stop}}
 	end
 
-	def fsm(state) do
-		case state do
-			:idle ->
-				floors = [0, 1, 2, 3]
-				button_types = [:hall_up, :hall_down, :cab]		
-				
-		end
+
+	def handle_call(:get_state, _from, state) do
+		{:reply, state, state}
 	end
+	
+	def handle_cast({:update, floor}, state) do
+		{_dontcare, goal_floor, direction} = state
+		{:noreply, {floor, goal_floor, direction}}
+	end
+
+	def get_state() do
+		GenServer.call(__MODULE__, :get_state)
+	end
+
+
+	def floor_sensor(floor) do
+		GenServer.cast(__MODULE__, {:update, floor})
+	end
+
+
+
+#Denne kan vi legge et annet sted helst:)
+	def initialize() do
+		{:ok, heisPID} = Driver.start
+		Driver.set_motor_direction(heisPID, :down)
+		{:ok, heisPID}
+	end
+
+
+
 end
