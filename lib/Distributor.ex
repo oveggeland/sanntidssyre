d3b48c0deaf6b4e009ad7219c6007d461de3c5f1
Oskar Veggeland <oveggeland@gmail.com>
2020-03-25 14:42:14 +0100

    Added Pinger module to maintain node-connections

--- lib/Distributor.ex
+++ lib/Distributor.ex
@@ -14,41 +14,18 @@ defmodule Distributor do
 
 	## API ##
 
-	def add_socket(node, socket) do
-		GenServer.cast(node, {:add_socket, socket})
-	end 
-
-	def get_sockets(node) do
-		GenServer.call(node, :get_sockets)
-	end
-
 	def new_order(order) do
 		bids = GenServer.call(__MODULE__, {:get_bids, order})
 		IO.inspect(bids)
 	end	
 
 	## Call handlers
-
-	def handle_call(:get_sockets, _from, sockets) do
-		{:reply, sockets, sockets}
-	end
-
 	def handle_call({:get_bids, order}, sockets) do
-		Enum.each(sockets, fn(socket) -> send(socket, {:get_bid, self(), order}) end)
+		#Enum.each(sockets, fn(socket) -> send(socket, {:get_bid, self(), order}) end)
+		IO.puts("get_bids not implemented yet")
 		:timer.sleep(1000)		
 	end
 
 	## Cast handlers
-
-	def handle_cast({:add_socket, socket}, sockets) do
-		sockets = [socket | sockets]
-		{:noreply, sockets}
-	end
-
-
-	def send_bid({from, order}) do
-		#Do some evaluation on the order and send bid back
-		send(from, {:send_bid, order*4})
-	end
 
 end
